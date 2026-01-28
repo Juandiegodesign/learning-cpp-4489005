c98ca6276b19edc3c2d0c14a52ed09856e0ad899
Juan Diego Bonilla <75050568+Juandiegodesign@users.noreply.github.com>
2026-01-28 20:27:08 +0000

    Added comment

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,14 @@
 // Learning C++ 
-// Exercise 01_02
+// Exercise 01_02 Modded
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hi There!" << std::endl;
+
+    std::cout << std::endl << std::endl;
+
+    return(0);
+
+}
